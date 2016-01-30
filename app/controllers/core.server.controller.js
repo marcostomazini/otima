dbaca7cb283f36fcfc4652f4313e11de50db32c0
marcos.tomazini@gmail.com <marcos.tomazini@gmail.com>
2016-01-30 01:35:57 -0200

    alter

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var RaspiCam = require("raspicam");
+var path = require('path');
+var async = require('async');
+var exec = require('child_process').exec;
 
 /**
  * Module dependencies.
@@ -14,21 +17,26 @@ exports.index = function(req, res) {
 
 
 exports.photo = function(req, res) {
-	var camera = new RaspiCam({
-		mode: "photo",
-		output: "/home/pi/teste.jpg",
-		w: 180
-	});
-	camera.start('timelapse');
 
-	camera.stop();
+	var spawn = require('child_process').spawn;
+	var raspistill = spawn('raspistill', ['-w', '640', '-h', '480', '-q', '5', '-o', '/home/pi/pic.jpg', '-tl', '1000', '-t', '9999999', '-th', '0:0:0', '-n', '-rot', '180']);
 
-	camera.on("read", function(err, timestamp, filename){ 
-    	//do stuff
-    	console.log(err);
-    	console.log(timestamp);
-    	console.log(filename);
-	});
+
+	// var camera = new RaspiCam({
+	// 	mode: "photo",
+	// 	output: "/home/pi/teste.jpg",
+	// 	w: 180
+	// });
+	// camera.start('timelapse');
+
+	// camera.stop();
+
+	// camera.on("read", function(err, timestamp, filename){ 
+ //    	//do stuff
+ //    	console.log(err);
+ //    	console.log(timestamp);
+ //    	console.log(filename);
+	// });
 
 	res.json({'type': 'success'});
 };
